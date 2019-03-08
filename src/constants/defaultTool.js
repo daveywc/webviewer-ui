26b3c7e183ccf7bd2db388fe6c697b46a0513ae8
David Compton <david@harboursoftware.com.au>
2019-03-08 11:23:28 +1100

    Changed the tool to 'Pan' for all platforms to enable the slider navigation to work immediately in web app.

--- src/constants/defaultTool.js
+++ src/constants/defaultTool.js
@@ -1,3 +1,6 @@
 import { isIOS, isAndroid } from 'helpers/device';
 
-export default (isIOS || isAndroid) ? 'Pan' : 'AnnotationEdit';
+//export default (isIOS || isAndroid) ? 'Pan' : 'AnnotationEdit';
+
+// Changed this so slider would work immediately without needing to click the 'Pan' button
+export default 'Pan';
